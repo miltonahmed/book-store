77ebcb60b6ca9c9a7681c793cf5acd8014a31293
Milton Ahmed <miltonahmed08@yahoo.com>
2025-12-29 16:28:25 +0400

    Navbar
    Fished navbar

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className='py'>
+      <div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Banner
--- src/Layouts/RootLayouts.jsx
+++ src/Layouts/RootLayouts.jsx
@@ -4,11 +4,17 @@ import Header from '../Components/Header/Header';
 
 const RootLayouts = () => {
   return (
-    <>
-      <Header/>
+    <div className="min-h-screen flex flex-col px-4">
+      <header className='w-full'>
+        <Header />
+      </header>
+      <main className="flex-1 max-w-11/12 mx-auto ">
         <Outlet />
-      <Footer />
-    </>
+      </main>
+      <footer className='w-full'>
+        <Footer />
+      </footer>
+    </div>
   );
 };
 
